15ec917381383888e6b657f0fa9fcf74e5795ffe
Roger Ransome <52100206+rog2054@users.noreply.github.com>
2025-04-11 16:51:53 +0100

    re-baseline migrations

---
+++ migrations/versions/123abc456def_baseline_schema.py
@@ -0,0 +1,18 @@
+"""Baseline schema
+Revision ID: 123abc456def
+Revises: 
+Create Date: 2025-04-11 15:00:00
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '123abc456def'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
