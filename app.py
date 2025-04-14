04f69e4d6c696b5b16b15cb35cc3a04a66a22520
Roger Ransome <52100206+rog2054@users.noreply.github.com>
2025-04-14 16:15:54 +0100

    remove input normalisation for lookup_transceiver_info_for_pid function as it isn't required

--- app.py
+++ app.py
@@ -1089,9 +1089,6 @@ def lookup_transceiver_info_for_pid(pid):
     "QSFP-100G-DR-S":      {"lanes": 1, "type": "SM", "distance": "500 m", "speed": "100G"}
     }
 
-    # normalise to remove vendor suffixes and -S etc
-    pid = pid.split("-")[0] + "-" + pid.split("-")[1]
-    logger.info(f"split pid within lookup_transceiver_info_for_pid: '{pid}'")
     return transceiver_info.get(pid) # returns None is no match found
 
 def parse_iosxe_transceiver_tx_rx(output):
